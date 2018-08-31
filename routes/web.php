4c4e6305bec3ab366c04dc99bdf4751443acba57
fengzi91 <fengzi91@vip.qq.com>
2018-09-01 07:52:14 +0800

    增加了话题功能框架

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
