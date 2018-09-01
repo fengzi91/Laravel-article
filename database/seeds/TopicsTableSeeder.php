c6260e812046625b9890de8437eb50111b5331fb
fengzi91 <fengzi91@vip.qq.com>
2018-09-01 08:16:07 +0800

    用户和话题数据的填充

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,15 +2,25 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function ($topic, $index) 
+                            use ($user_ids, $faker) 
+        {
+          // 从用户 ID 数组中随机取出一个并赋值
+          $topic->user_id = $faker->randomElement($user_ids); 
         });
 
         Topic::insert($topics->toArray());
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        $users = factory(User::class)->times(10)->make();
+
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        User::insert($user_array);
+
+        $user = User::find(1);
+        $user->name = 'fengzi91';
+        $user->email = 'fengzi91@vip.qq.com';
+        // $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+    }
+}
