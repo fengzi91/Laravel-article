8fb569a850714755e994ba623fb17e26f961a024
fengzi91 <fengzi91@vip.qq.com>
2018-09-09 23:43:35 +0000

    修正一个大小写错误

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\User;
-use App\jobs\CreateUserAvatar;
+use App\Jobs\CreateUserAvatar;
 
 use Illuminate\Support\Facades\Log;
 // creating, created, updating, updated, saving,
