38fc07cc1016fe0105ba1f216c67b67f28c26f49
fengzi91 <fengzi91@vip.qq.com>
2019-02-27 08:18:05 +0000

    修改默认密码

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,20 +17,20 @@ class UsersTableSeeder extends Seeder
         $users = factory(User::class)->times(80)->make();
 
         $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
-        
+
         User::insert($user_array);
 
         $user = User::find(1);
         $user->name = 'fengzi91';
         $user->email = 'fengzi91@vip.qq.com';
-        $user->password = bcrypt('123000qq');
+        $user->password = bcrypt('123456');
         $user->assignRole('Founder');
         $user->save();
         // 将 2 号用户指派为『管理员』
         $user = User::find(2);
         $user->name = 'fengzi92';
         $user->email = '393011655@qq.com';
-        $user->password = bcrypt('123000qq');
+        $user->password = bcrypt('123456');
         $user->assignRole('Maintainer');
         $user->save();
     }
